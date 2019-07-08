ce99945b95413a6f191f50cf2fc7055a2c28d3e4
DaGuT <mangustalex11@gmail.com>
2019-07-08 16:48:02 +0700

    partially working MRMR with my framework support

--- skfeature/function/information_theoretical_based/MRMR.py
+++ skfeature/function/information_theoretical_based/MRMR.py
@@ -1,5 +1,5 @@
 from skfeature.function.information_theoretical_based import LCSI
-
+import numpy as np
 
 def mrmr(X, y, **kwargs):
     """
@@ -34,3 +34,47 @@ def mrmr(X, y, **kwargs):
     else:
         F, J_CMI, MIfy = LCSI.lcsi(X, y, gamma=0, function_name='MRMR')
     return F, J_CMI, MIfy
+
+class MRMR:
+    def __init__(self,n_selected_features=None):
+        if n_selected_features!=None:
+            self.n_selected_features = n_selected_features
+
+    def fit(self,X,y):
+
+        #just empty array of selected indeces
+        support_ = []
+
+        #we get vector-mask with selected features
+        if hasattr(self, "n_selected_features"):
+            support_,self.J_CMI, self.MIfy = mrmr(X,y,n_selected_features=self.n_selected_features)
+        else:
+            support_,self.J_CMI, self.MIfy = mrmr(X,y)
+
+        #array of the same length as we have number of features, filled with FALSE
+        self.support_=np.zeros(X.shape[-1], dtype=bool)
+        #and we change false -> True on positions which features were selected
+        self.support_[support_]=True
+
+        print(support_)
+        print(support_.shape)
+
+
+        #we set num of features
+        self.n_features_ = self.support_.sum()
+
+        return self
+
+    def transform(self,X):
+
+        #we check if we have fitted it
+        if not hasattr(self, "support_"):
+            print("Fit your selector first! All features were returned.")
+            return X
+
+        #we extract seleted features
+        return X[self.support_]
+
+    def get_support(self):
+
+        return self.support_
